120a6d7e779ec9d0eed37cb2fc001a55da2339bb
gaurav <gsharma774962@gmail.com>
2022-03-10 22:14:03 +0530

    buildpack

--- src/Store/store.js
+++ src/Store/store.js
@@ -6,10 +6,7 @@ import createSagaMiddleware from "redux-saga";
 const sagaMiddleWare = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(sagaMiddleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(sagaMiddleWare))
 );
 sagaMiddleWare.run(homePageSaga);
 export default store;
